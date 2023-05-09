3505a33cc18cbb4ad8d0d8427b999d92d832e1a3
Richard Foard <rfoard@richardfoard.com>
2023-05-08 21:51:33 -0400

    Add topo-to-geom coordinate conversion tester

---
+++ qrs2xy.py
@@ -0,0 +1,23 @@
+import math
+import sys
+
+def main():
+    if len(sys.argv) != 4:
+        print(f'Please use {sys.argv[0]} <q> <r> <s>')
+        sys.exit(1)
+
+    q = float(sys.argv[1])
+    r = float(sys.argv[2])
+    s = float(sys.argv[3])
+
+    root3_2 = math.sqrt(3) / 2.0
+    scale = 5.0
+
+    x = scale * (r + q/2.0 + s/2.0)
+    y = scale * (q * root3_2 - s * root3_2)
+
+    print(f'(q, r, s) = ({q}, {r}, {s}) ==> (x, y) = ({x}, {y})')
+
+
+if __name__ == '__main__':
+    main()
